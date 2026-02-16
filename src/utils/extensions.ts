d03fd854ae249559c0c8e0ec17546e8cbb002e87
keturn <83819+keturn@users.noreply.github.com>
2026-02-16 05:26:50 +0000

    api: add chat.getMessage and chat.getHistoryLength for much faster access

--- src/utils/extensions.ts
+++ src/utils/extensions.ts
@@ -223,6 +223,9 @@ const baseExtensionAPI: ExtensionAPI = {
       if (!store.activeChat) throw new Error('No active chat.');
       return deepClone(store.activeChat.messages);
     },
+    getHistoryLength: () => {
+      return useChatStore().activeChat?.messages.length ?? 0;
+    },
     getChatInfo: () => {
       const store = useChatStore();
       return deepClone(
@@ -231,6 +234,10 @@ const baseExtensionAPI: ExtensionAPI = {
     },
     getAllChatInfos: () => {
       return deepClone(useChatStore().chatInfos);
+    },
+    getMessage: (index: number) => {
+      const messages = useChatStore().activeChat?.messages ?? [];
+      return deepClone(messages[index]);
     },
     getLastMessage: () => {
       const messages = useChatStore().activeChat?.messages ?? [];
